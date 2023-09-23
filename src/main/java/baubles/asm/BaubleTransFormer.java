1752b837eabc00c1b1e4fc20c18ddaf2d1f3fcd8
Ventivu <3123632012@qq.com>
2023-09-23 17:38:58 +0800

    针对etfuturm的修复，顺道说一句真够傻逼的

--- src/main/java/baubles/asm/BaubleTransFormer.java
+++ src/main/java/baubles/asm/BaubleTransFormer.java
@@ -1,29 +1,31 @@
 package baubles.asm;
 
-import com.google.common.io.Files;
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
-import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.Level;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.tree.AnnotationNode;
 import org.objectweb.asm.tree.ClassNode;
+import ventivu.core.Core.DeployError;
+import ventivu.core.Core.Reason;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.*;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
-import java.util.jar.JarInputStream;
-import java.util.jar.JarOutputStream;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
 public class BaubleTransFormer implements IFMLLoadingPlugin {
+    boolean hasChanged = false;
 
     @Override
     public String[] getASMTransformerClass() {
@@ -45,6 +47,7 @@ public class BaubleTransFormer implements IFMLLoadingPlugin {
      */
     @Override
     public void injectData(Map<String, Object> data) {
+        byte[] bytes = new byte[0];
         List<File> files = new ArrayList<>();
         File me = (File) data.get("coremodLocation");
         ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
@@ -58,16 +61,14 @@ public class BaubleTransFormer implements IFMLLoadingPlugin {
             if (f != null) files.addAll(Arrays.asList(f));
         }
         //生成API描述文件
-        try (JarFile myjar = new JarFile(me)) {
-            InputStream stream = myjar.getInputStream(myjar.getEntry("baubles/api/package-info.class"));
-            ClassReader reader = new ClassReader(stream);
+        try (JarFile myjar = new JarFile(me); InputStream stream = myjar.getInputStream(myjar.getEntry("baubles/api/package-info.class"));) {
             ClassNode node = new ClassNode();
-            reader.accept(node, 0);
+            new ClassReader(stream).accept(node, 0);
             AnnotationNode annotation = node.visibleAnnotations.get(0);
             for (int i = 0; i < annotation.values.size(); i++)
                 if ("apiVersion".equals(annotation.values.get(i))) annotation.values.set(i + 1, "1.0.0.0");
             node.accept(cw);
-
+            bytes = cw.toByteArray();
         } catch (Exception e) {
             FMLLog.log(Level.ERROR, e, "为什么读取不了自己?");
         }
@@ -77,40 +78,52 @@ public class BaubleTransFormer implements IFMLLoadingPlugin {
                 JarEntry entry = jar.getJarEntry("baubles/api");
                 if (entry != null) {
                     if (jar.getJarEntry("baubles/api/package-info.class") != null) continue;
-
-                    File temp = File.createTempFile(mod.getName(), null, mod.getParentFile());
-                    Files.copy(mod, temp);
-                    JarInputStream source = new JarInputStream(java.nio.file.Files.newInputStream(temp.toPath()));
-                    JarOutputStream jos = new JarOutputStream(java.nio.file.Files.newOutputStream(mod.toPath()));
-                    ZipEntry e;
-                    while ((e = source.getNextEntry()) != null) {
-                        ZipEntry zosEntry = new ZipEntry(e);
-                        zosEntry.setCompressedSize(-1);
-                        Field field = ZipEntry.class.getDeclaredField("crc");
-                        field.setAccessible(true);
-                        field.set(zosEntry, -1);
-                        field = ZipEntry.class.getDeclaredField("size");
-                        field.setAccessible(true);
-                        field.set(zosEntry, -1);
-
-                        zosEntry.setComment(e.getComment());
-                        zosEntry.setExtra(e.getExtra());
-                        jos.putNextEntry(zosEntry);
-                        IOUtils.copy(source, jos);
-                    }
-
-                    JarEntry out = new JarEntry("baubles/api/package-info.class");
-                    jos.putNextEntry(out);
-                    jos.write(cw.toByteArray());
-                    jos.closeEntry();
-                    jos.close();
-                    source.close();
-                    temp.delete();
+                    injectJar(jar, bytes, mod.getParentFile());
                 }
             } catch (Exception e) {
-                FMLLog.log(Level.ERROR, e, "读取文件%s异常", mod);
+                FMLLog.log(Level.ERROR, e, "操作文件%s异常", mod);
             }
         }
+        if (hasChanged) throw new DeployError(Reason.FORTRACE.setReasonMessage("这是一次计划性报错，请再启动一次"));
+    }
+
+    private void injectJar(ZipFile jar, byte[] packageinfo, File directory) throws IOException {
+        hasChanged = true;
+        File temp = File.createTempFile(jar.getName(), null, directory);
+        temp.deleteOnExit();
+        Files.copy(Paths.get(jar.getName()), temp.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        boolean planB = false;
+        while (true)
+            try (ZipFile zip = new ZipFile(temp); ZipOutputStream zos = new ZipOutputStream(java.nio.file.Files.newOutputStream(Paths.get(jar.getName())))) {
+                Enumeration<? extends ZipEntry> entries = zip.entries();
+                while (entries.hasMoreElements()) {
+                    ZipEntry entry = entries.nextElement();
+                    if (entry.isDirectory()) continue;
+                    zos.putNextEntry(planB ? new ZipEntry(entry.getName()) : new ZipEntry(entry));
+
+                    try (InputStream is = zip.getInputStream(entry)) {
+                        for (int l = is.read(); l > -1; l = is.read()) zos.write(l);
+                    }
+                    zos.closeEntry();
+                    zos.flush();
+                }
+                addInfo(packageinfo, zos);
+                zip.close();
+                temp.delete();
+                break;
+            } catch (ZipException e) {
+                if (planB)
+                    throw new DeployError(Reason.LoadError.setReasonMessage("不支持的特殊压缩方案:" + jar.getName()));
+                planB = true;
+            }
+    }
+
+    private void addInfo(byte[] info, ZipOutputStream zos) throws IOException {
+        ZipEntry out = new ZipEntry("baubles/api/package-info.class");
+        zos.putNextEntry(out);
+        zos.write(info);
+        zos.closeEntry();
+        zos.flush();
     }
 
     @Override
